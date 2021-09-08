a46b1d80874e6d3477ea5e946e51185ca85f868d
Alex Figueroa <trance4life503@gmail.com>
2021-09-08 17:08:34 -0600

    generar matriz solo de numeros

--- examen.h
+++ examen.h
@@ -2,14 +2,15 @@
 #ifndef EXAMEN_H
 #define EXAMEN_H
 
-#define BUF_SIZE 1024 
+#define BUF_SIZE 2048 
 
 int getrows(FILE *);
 int getcolumns(FILE *);
-void readcsv(const char *);
+void readcsv(const char);
 const char* integrantes();
 int *transpose(int *, int, int);
 int *multiplyMatrices(int *first_array,int first_row, int first_column, int *second_array, int second_row, int second_column);
+int *generateMatrices(const char * );
 
 #endif
 
