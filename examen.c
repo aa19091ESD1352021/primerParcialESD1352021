68bbf42a248992f5396d52448eb240daea9a295c
Alex Figueroa <trance4life503@gmail.com>
2021-09-08 18:21:30 -0600

    leer csv

--- examen.c
+++ examen.c
@@ -5,138 +5,176 @@
 
 int getrows(FILE * pointer_file) {
 
-  size_t row_counter = 1;
-  char buffer = fgetc(pointer_file);
+    size_t row_counter = 1;
+    char buffer = fgetc(pointer_file);
 
-  while (!feof(pointer_file)) {
+    while (!feof(pointer_file)) {
 
-    if (buffer == '\n')
-      row_counter++;
+        if (buffer == '\n')
+            row_counter++;
 
-    buffer = fgetc(pointer_file);
-  }
+        buffer = fgetc(pointer_file);
+    }
 
-  return row_counter;
+    return row_counter;
 }
 
 int getcolumns(FILE * pointer_file) {
-  size_t column_counter = 1;
+    size_t column_counter = 1;
 
-  char buffer[BUF_SIZE];
-  if (fgets(buffer, BUF_SIZE, pointer_file)) {
+    char buffer[BUF_SIZE];
+    if (fgets(buffer, BUF_SIZE, pointer_file)) {
 
-    int i;
-    for (i = 0; i < strlen(buffer) - 1; i++) {
+        int i;
+        for (i = 0; i < strlen(buffer) - 1; i++) {
 
-      if (buffer[i] == ',') {
-        column_counter++;
-      }
+            if (buffer[i] == ',') {
+                column_counter++;
+            }
+
+        }
 
     }
 
-  }
-
-  return column_counter;
+    return column_counter;
 }
 
-void readcsv(const char path) {
+char *readcsv(const char *path) {
+
+    FILE *pointer_file = fopen(path, "r");
+    if (pointer_file == NULL) {
+        puts("error");
+        return NULL;
+    }
+    int rows = 1;
+    int columns = 1;
+    while (feof(pointer_file) == 0) {
+        char character = fgetc(pointer_file);
+        if (character == '\n') {
+            rows++;
+        } else if (rows < 2 && character == 44) {
+            columns++;
+        }
+    }
+
+    rewind(pointer_file);
+    int i = 0;
+    char *almacenamiento;
+    char space[1024];
+    int columnas = 0;
+    dataType *arrays;
+    arrays = (dataType *) malloc(rows * columns * sizeof (dataType));
+    while (i < rows) {
+        columnas = 0;
+        fgets(space, 10000, pointer_file);
+        almacenamiento = strtok(space, ",");
+        while (almacenamiento != NULL) {
+            strcpy(*(arrays + i * columns + columnas), almacenamiento);
+            almacenamiento = strtok(NULL, ",");
+            columnas++;
+        }
+        i++;
+    }
+    puts("");
+    fclose(pointer_file);
+    return arrays;
 
 }
 
 const char * integrantes() {
-  static char grupo[] = "AA19091,RF18025";
-  return grupo;
+    static char grupo[] = "AA19091,RF18025";
+    return grupo;
 }
 
 int * transpose(int *array, int rows, int columns) {
-  int i;
-  int j;
+    int i;
+    int j;
 
-  int *transpose_array = (int *) malloc(columns * rows * sizeof (int));
-  // se debe liberar la memoria despues de usar la funcion! 
+    int *transpose_array = (int *) malloc(columns * rows * sizeof (int));
+    // se debe liberar la memoria despues de usar la funcion! 
 
-  for (i = 0; i < rows; i++) {
+    for (i = 0; i < rows; i++) {
 
-    for (j = 0; j < columns; j++) {
-      *(transpose_array + j * rows + i) = *(array + i * columns + j);
+        for (j = 0; j < columns; j++) {
+            *(transpose_array + j * rows + i) = *(array + i * columns + j);
+        }
     }
-  }
 
-  return transpose_array;
+    return transpose_array;
 }
 
 int *multiplyMatrices(int *first_array, int first_row, int first_column, int *second_array, int second_row, int second_column) {
 
-  if (first_column == second_row) {
-    int aux;
-    int *result = (int*) malloc(first_row * second_column * sizeof (int));
-    for (int i = 0; i < first_row; i++) {
-      for (int j = 0; j < second_column; j++) {
-        aux = 0;
-        for (int x = 0; x < first_column; x++) {
-          aux += (*(first_array + i * first_column + x)) * (*(second_array + x * second_column + j));
+    if (first_column == second_row) {
+        int aux;
+        int *result = (int*) malloc(first_row * second_column * sizeof (int));
+        for (int i = 0; i < first_row; i++) {
+            for (int j = 0; j < second_column; j++) {
+                aux = 0;
+                for (int x = 0; x < first_column; x++) {
+                    aux += (*(first_array + i * first_column + x)) * (*(second_array + x * second_column + j));
+                }
+                *(result + i * second_column + j) = aux;
+            }
         }
-        *(result + i * second_column + j) = aux;
-      }
+        return result;
+    } else {
+        puts("dimensiones incorrectas");
+        exit(0);
     }
-    return result;
-  } else {
-    puts("dimensiones incorrectas");
-    exit(0);
-  }
 }
 
 int *generateMatrices(const char * path) {
 
-  FILE *pointer_file;
-  pointer_file = fopen(path, "r");
-  if (pointer_file == NULL) {
-    puts("Error no se pudo abrir el archivo");
-    exit(1);
-
-  }
-
-
-  char temp[BUF_SIZE];
-  char aux;
-  char *character;
-  int rows = 0;
-  int columns = 0;
-
-  while (!feof(pointer_file)) {
-    fgets(temp, BUF_SIZE, pointer_file);
-    rows++;
-  }
-
-  rewind(pointer_file);
-  while (aux != '\n') {
-    aux = fgetc(pointer_file);
-
-    if (aux == 44) {
-      columns++;
-    }
-  }
-
-  int *array = (int *) malloc((rows - 1)*(columns) * sizeof (int));
-  for (int i = 0; i < (rows - 1); i++) {
-    int j = 0;
-    while (aux != ',') {
-      aux = fgetc(pointer_file);
+    FILE *pointer_file;
+    pointer_file = fopen(path, "r");
+    if (pointer_file == NULL) {
+        puts("Error no se pudo abrir el archivo");
+        exit(1);
 
     }
 
-    while (aux != '\n' && aux != EOF) {
-      aux = fgetc(pointer_file);
-      if (aux != ',' && aux != EOF && aux != '\n') {
-        character = &aux;
-        *(array + i * columns + j) = atoi(character);
-        j++;
-      }
-    }
-  }
 
-  fclose(pointer_file);
-  return array;
+    char temp[BUF_SIZE];
+    char aux;
+    char *character;
+    int rows = 0;
+    int columns = 0;
+
+    while (!feof(pointer_file)) {
+        fgets(temp, BUF_SIZE, pointer_file);
+        rows++;
+    }
+
+    rewind(pointer_file);
+    while (aux != '\n') {
+        aux = fgetc(pointer_file);
+
+        if (aux == 44) {
+            columns++;
+        }
+    }
+
+    int *array = (int *) malloc((rows - 1)*(columns) * sizeof (int));
+    for (int i = 0; i < (rows - 1); i++) {
+        int j = 0;
+        while (aux != ',') {
+            aux = fgetc(pointer_file);
+
+        }
+
+        while (aux != '\n' && aux != EOF) {
+            aux = fgetc(pointer_file);
+            if (aux != ',' && aux != EOF && aux != '\n') {
+                character = &aux;
+                *(array + i * columns + j) = atoi(character);
+                j++;
+            }
+        }
+    }
+
+    fclose(pointer_file);
+    return array;
 }
 
 
--- examen.h
+++ examen.h
@@ -4,13 +4,15 @@
 
 #define BUF_SIZE 2048 
 
+typedef char dataType [2048];
+
 int getrows(FILE *);
 int getcolumns(FILE *);
-void readcsv(const char);
+char *readcsv(const char *);
 const char* integrantes();
 int *transpose(int *, int, int);
-int *multiplyMatrices(int *first_array,int first_row, int first_column, int *second_array, int second_row, int second_column);
-int *generateMatrices(const char * );
+int *multiplyMatrices(int *first_array, int first_row, int first_column, int *second_array, int second_row, int second_column);
+int *generateMatrices(const char *);
 
 #endif
 
